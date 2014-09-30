56f303877cb4f3e5762ae8e43cfc9e83e5324283
Harshul Gandhi <gandhi.harshul.88@gmail.com>
2014-10-01 03:34:17 +0800

    Planner algorithm second commit

--- Assignment 2/planning.py
+++ Assignment 2/planning.py
@@ -1,3 +1,7 @@
+import math
+import matplotlib.pyplot as plt
+
+
 start = [0.0,0.0,0.0]
 goal = [1.0,1.0,1.57]
 obstacles = [(0.4,0.6),(0.4,0.4),(0.6,0.4),(0.6,0.6)]
@@ -28,6 +32,52 @@ print "Number of samples formed : ",len(graph_sample)
 '''graph_edges = [(graph_sample[0],graph_sample[1]),(graph_sample[0],graph_sample[2]),(graph_sample[0],graph_sample[3]),(graph_sample[0],graph_sample[4]),(graph_sample[0],graph_sample[5])]
 '''
 
+#to plot a list of (x,y) coordinates on graph
+def plotSamplePoints(listxy):
+	x = []
+	y = []
+	for i in range(len(listxy)):
+		x.append(listxy[i][0])
+	        y.append(listxy[i][1])	
+	plt.scatter(x,y)
+	plt.savefig('Sample Points.jpg')
+
+#function to calculate distance between two points
+def lengthOfAnEdge(edge):
+	x1 = edge[0][0]
+	y1 = edge[0][1]
+	x2 = edge[1][0]
+	y2 = edge[1][1]
+	
+	return round(((x2-x1)**2+(y2-y1)**2)**0.5)
+
+
+#calculate the slope of an edge between (x1,y1) and (x2,y2) wrt to x-axis
+def slopeOfAnEdge(edge):
+	x1 = edge[0][0]
+        y1 = edge[0][1]
+        x2 = edge[1][0]
+        y2 = edge[1][1]
+	
+	return math.atan((y2-y1)/(x2-x1))
+
+
+#function to identify and remove all the illegal edges from the graph
+def removeIllegalEdges(graphEdge,graph_sample):
+	i = 0
+	dynamicLengthGraphEdge = len(graphEdge)
+	while (i < (dynamicLengthGraphEdge-1)):
+		for j,k in zip(range(2,len(graph_sample)-2,2),range(3,len(graph_sample)-2,2)):
+			if(graphEdge[i][0]==graph_sample[j] and graphEdge[i][1] == graph_sample[j+2]):
+				graphEdge.remove(graphEdge[i])
+				dynamicLengthGraphEdge = dynamicLengthGraphEdge - 1
+			if(graphEdge[i][0]==graph_sample[k] and graphEdge[i][1] == graph_sample[k+2]):
+				graphEdge.remove(graphEdge[i])
+				dynamicLengthGraphEdge = dynamicLengthGraphEdge - 1
+		i += 1
+		
+	return graphEdge		
+	
 
 #function to form all possible edges from a list of samples given
 def getEdges(graph_sample):
@@ -39,7 +89,15 @@ def getEdges(graph_sample):
 graphEdge = []
 getEdges(graph_sample)
 
-print "EDGES :",graphEdge		
+print "EDGES :",graphEdge
+#print "EDGE[0][0][0] : ",graphEdge[0][0][0]
+
+removeIllegalEdges(graphEdge,graph_sample)
+print "EDGES after removing illegal ones :",graphEdge	
+
+print "Lenght of edge ",graphEdge[1]," is ",lengthOfAnEdge(graphEdge[1])
+print "Slope of edge ",graphEdge[1]," is ",slopeOfAnEdge(graphEdge[1])
+plotSamplePoints(graph_sample)
 
 def getSizeOfGraphEdgeList(num):
         factorial = 1
