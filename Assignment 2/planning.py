5863e8b9dcd720bed0175b2d84b74a9dfbc86269
Harshul Gandhi <gandhi.harshul.88@gmail.com>
2014-10-09 11:10:35 +0800

    Path planning with Dijkstra's algorithm

--- Assignment 2/planning.py
+++ Assignment 2/planning.py
@@ -183,6 +183,9 @@ def removeIllegalEdgesInsideObstacle(graphEdge,obstacleEdge):
 		for j in range(len(obstacleEdge)):
 #			print "i = ",i,"    and j = ",j
 			if(areTwoLinesIntersecting(graphEdge[i],obstacleEdge[j])):
+#				print "Edges checked for interesection : ",graphEdge[i],obstacleEdge[j]
+#				print "Are intersecting ? : ",areTwoLinesIntersecting(graphEdge[i],obstacleEdge[j])
+#				print "Edge that will be removed : ",graphEdge[i]
 				listOfIndexToBeRemoved.append(i)
 #				lengthOfGraphEdge -= 1
 #				print "Graph edge removed is : ",graphEdge[i]
@@ -190,9 +193,11 @@ def removeIllegalEdgesInsideObstacle(graphEdge,obstacleEdge):
 		i += 1
 	
 	for k in range(len(listOfIndexToBeRemoved)):
+		
 		graphEdge.remove(graphEdge[listOfIndexToBeRemoved[k-countOfEdgesRemoved]])
 		countOfEdgesRemoved += 1
-
+	
+	print "countOfEdgesRemoved :",countOfEdgesRemoved
 	return graphEdge			
 
 #function to form all possible edges from a list of samples given
@@ -204,29 +209,36 @@ def getEdges(graph_sample):
 
 #To check if three points are aligned in clockwise or anticlockwise orientation. Also if they are collinear.
 def arePointsCCW(point1,point2,point3):
-	if (((point2[0] - point1[0])*(point3[1]-point2[1]) - (point3[0]-point2[0])*(point2[1]-point1[1])) == 0):
-		return False
-	else:
-		return (((point2[0] - point1[0])*(point3[1]-point2[1]) - (point3[0]-point2[0])*(point2[1]-point1[1])) < 0)
+#	if (((point2[0] - point1[0])*(point3[1]-point2[1]) - (point3[0]-point2[0])*(point2[1]-point1[1])) == 0):
+#		return False
+#	else:
+	return ((point3[1] - point1[1])*(point2[0]-point1[0]) > (point2[1]-point1[1])*(point3[0]-point1[0]))
 
 
 #function to check if two line segments are intersecting or not
 def areTwoLinesIntersecting(line1,line2):
-	return ((arePointsCCW(line1[0],line1[1],line2[0]))^(arePointsCCW(line1[0],line1[1],line2[1]))) and ((arePointsCCW(line2[0],line2[1] ,line1[0]))^(arePointsCCW(line2[0],line2[1],line1[1])))	
-''' FOR TESTING
+	'''FOR TESTING
 	print "Point 3 and 4  wrt point 1 and 2 : ",(arePointsCCW(line1[0],line1[1],line2[0]))^(arePointsCCW(line1[0],line1[1],line2[1]))
 	print "Point 1 and 2 with respect to 3 and 4 : ",(arePointsCCW(line2[0],line2[1],line1[0]))^(arePointsCCW(line2[0],line2[1],line1[1]))
 	print "arePointsCCW(line1[0],line1[1],line2[0]) : ",arePointsCCW(line1[0],line1[1],line2[0])
 	print "arePointsCCW(line1[0],line1[1],line2[1]) : ",arePointsCCW(line1[0],line1[1],line2[1])
 	print "arePointsCCW(line2[0],line2[1],line1[0]) : ",arePointsCCW(line2[0],line2[1],line1[0])
 	print "arePointsCCW(line2[0],line2[1],line1[1]) : ",arePointsCCW(line2[0],line2[1],line1[1])'''
+#	return ((arePointsCCW(line1[0],line1[1],line2[0]))^(arePointsCCW(line1[0],line1[1],line2[1]))) and ((arePointsCCW(line2[0],line2[1] ,line1[0]))^(arePointsCCW(line2[0],line2[1],line1[1])))
 	
-'''
-	if (arePointsCCW(line1[0],line1[1],line2[0]) and not(arePointsCCW(line1[0],line1[1],line2[1]))) and  (not((arePointsCCW(line1[0],line1[1],line2[0])) and arePointsCCW(line1[0],line1[1],line2[1]))):
-		return (2>1)
+#	return (arePointsCCW(line1[0],line2[0],line2[1]) != arePointsCCW(line1[1],line2[0],line2[1])) and (arePointsCCW(line1[0],line1[1],line2[0]) != arePointsCCW(line1[0],line1[1],line2[1]))
+	if((arePointsCCW(line1[0],line2[0],line2[1]) != arePointsCCW(line1[1],line2[0],line2[1])) and (arePointsCCW(line1[0],line1[1],line2[0]) != arePointsCCW(line1[0],line1[1],line2[1]))):
+		if(line1[0] == line2[0] or line1[1] == line2[0] or line1[0] == line2[1] or line1[1] == line2[1]):
+			return False
+		else:
+			 return True	
 	else:
-		return (1>2)
-	'''
+		return False
+#if (arePointsCCW(line1[0],line1[1],line2[0]) and not(arePointsCCW(line1[0],line1[1],line2[1]))) and  (not((arePointsCCW(line1[0],line1[1],line2[0])) and arePointsCCW(line1[0],line1[1],line2[1]))):
+#		return (2>1)
+#	else:
+#		return (1>2)
+
 
 
 removeIllegalEdgesObstacleDiagonal(graphEdge,graph_sample)
@@ -236,7 +248,7 @@ removeIllegalEdgesObstacleDiagonal(graphEdge,graph_sample)
    [(start),(goal),(all obstacles)].
    Third functionc gives the list of all the samples as visibility graph '''
 
-##Remove the comment after test
+#Remove the comment after test
 print getGraphSamples(getAllCoordinates(getTestFileInput('test3.mp')))  
 
 
@@ -253,6 +265,8 @@ removeIllegalEdgesInsideObstacle(graphEdge,obstacleEdge)
 print graphEdge
 print "Number of EDGES REMAINED :",len(graphEdge)
 
+
+
 #print "EDGES :",graphEdge
 #print "EDGE[0][0][0] : ",graphEdge[0][0][0]
 
@@ -267,10 +281,11 @@ print "Number of EDGES REMAINED :",len(graphEdge)
 #Cases where one end point of one of the line segments lies on the other 
 #segment, they are not considered to be in collision
 
+'''
 point1 = [0,0]
-point2 = [0.48,0.72]
-point3 = [0.28,0.52]
-point4 = [0.48,0.52]
+point2 = [3,5]
+point3 = [1,7]
+point4 = [3,5]
 line1 = [point1,point2]
 line2 = [point3,point4]
 print line1[0]
@@ -280,12 +295,88 @@ print line2[1]
 print arePointsCCW(point1,point2,point3)
 #print "test : ",arePointsCCW((2,1),(6,4),(3,3))
 print "Are line segments intersecting ",areTwoLinesIntersecting(line1,line2)
+'''
 
-def getSizeOfGraphEdgeList(num):
-        factorial = 1
-        while (num>=1):
-                factorial = factorial+num
-                num = num - 1
-        return factorial
+## Applying DIJKSTRA's ALGORITHM ##
+
+graph_sampleDistancePrevious = []
+
+
+#function to find out node with minimum distance value from graph_sampleDistancePrevious list
+def minDistanceSampleIndex(graph_sampleDistancePrevious):
+	minIndex = 0
+	minDistance = 0.0
+	for i in range(len(graph_sampleDistancePrevious)):
+		if(minDistance >= graph_sampleDistancePrevious[i][1]):
+			minDistance = graph_sampleDistancePrevious[i][1]
+			minIndex = i
+
+	return minIndex
+
+#function to return a list of neighbouring vertices for a point
+def getNeighbourOfASample(samplePoint,neighbourVertex):
+	for i in range(len(graphEdge)):
+		if(graphEdge[i][0] == samplePoint):
+			neighbourVertex.append(graphEdge[i][1])
+		elif(graphEdge[i][1] == samplePoint):
+			neighbourVertex.append(graphEdge[i][0])
+
+	return neighbourVertex
+	
+
+#function to return index of a (x,y) point from a list
+def getIndexOfAPointFromPredGraph(point,graph_sampleDistancePrevious):
+	
+	for i in range(len(graph_sampleDistancePrevious)):
+		if(graph_sampleDistancePrevious[i][0] == point):
+			return i
+
+
+#Dijktra's algorithm
+def dijkstra(graph_sample,graphEdge,start,goal):
+	unvisitedSample = []
+	print "len(graph_sample) : ",len(graph_sample)
+	for i in range(len(graph_sample)):
+		if(i == 0):
+			temp = (graph_sample[i],0.0,(9.9,9.9))
+			graph_sampleDistancePrevious.append(temp)
+			unvisitedSample.append(graph_sample[i])
+
+		if(i > 0):
+			temp = (graph_sample[i],999.9,(9.9,9.9))
+			graph_sampleDistancePrevious.append(temp)
+			unvisitedSample.append(graph_sample[i])
+		#graph_sampleDistancePrevious[i][0] = graph_sample[i]
+		#graph_sampleDistancePrevious[i][1] = 999.9
+		#graph_sampleDistancePrevious[i][2] = (9.9,9.9)
+	
+#	print "Printing graph_sampleDistancePrevious[0][1] from inside Dijkstra's: ",graph_sampleDistancePrevious[0][1]		
+	k = 0	
+	while(len(unvisitedSample)>0):
+		neighbourVertex = []
+		minDistanceIndex = minDistanceSampleIndex(graph_sampleDistancePrevious) 		
+		neighbourVertex = getNeighbourOfASample(graph_sampleDistancePrevious[i][0],neighbourVertex)
+		unvisitedSample.remove(graph_sampleDistancePrevious[minDistanceIndex][0])
+		for i in range(len(neighbourVertex)):
+			tempDistance = graph_sampleDistancePrevious[k][1] + lengthOfAnEdge([graph_sampleDistancePrevious[k][0],neighbourVertex[i]])			
+			vertexIndexInGraph = getIndexOfAPointFromPredGraph(neighbourVertex[i],graph_sampleDistancePrevious)
+			if(tempDistance < graph_sampleDistancePrevious[vertexIndexInGraph][1]):
+				graph_sampleDistancePrevious[vertexIndexInGraph][1] = tempDistance
+				graph_sampleDistancePrevious[vertexIndexInGraph][2] = graph_sampleDistancePrevious[k][0] 
+			
+			if(graph_sampleDistancePrevious[vertexIndexInGraph][2]==goal):
+				return graph_sampleDistancePrevious
+
+
+				
+		
+		
+	
+
+
+
+		
+#print "minDistanceSampleIndex(graph_sampleDistancePrevious) ",minDistanceSampleIndex(graph_sampleDistancePrevious)	
+print dijkstra(graph_sample,graphEdge,start,goal)
 
 
